bc0c7fd06d8641c233394042ee13cd77e62e068a
ashcolor <ashcolor27@gmail.com>
2022-05-12 10:07:42 +0900

    del: inspector削除

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
-import Inspector from "vite-plugin-vue-inspector";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,6 +23,5 @@ export default defineConfig({
         Components({
             dts: "./src/components.d.ts",
         }),
-        Inspector(),
     ],
 });
